a455046585198db3cb468414d36da57c1f014a8c
Soumil26 <75134006+Soumil26@users.noreply.github.com>
2025-10-07 22:57:58 +0530

    Update vite.config.ts

--- soumil-jee/vite.config.ts
+++ soumil-jee/vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: For project Pages, base must equal the repo name
 export default defineConfig({
 plugins: [react()],
-base: '/soumil-jee/',
+base: '/jee-sd-site/',
 })
